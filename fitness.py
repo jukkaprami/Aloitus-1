78c30e708a7dc1a9a03e38906d56ead28011aaff
Mika Vainio <koti.mika.vainio@gmail.com>
2023-01-20 14:58:48 +0200

    Lisätty lasten laskentakaava

--- fitness.py
+++ fitness.py
@@ -4,14 +4,14 @@
 # Muuttujat
 
 # Kysytään käyttäjältä tiedot
-pituus_teksti = input('Kuinka pitkä olet (cm): ') 
-paino_teksti = input('Kuinka paljon painat (kg): ') 
+pituus_teksti = input('Kuinka pitkä olet (cm): ')
+paino_teksti = input('Kuinka paljon painat (kg): ')
 ika_teksti = input('Kuinka vanha olet: ')
 sukupuoli_teksti = input('Sukupuoli mies, vastaa 1, nainen vastaa 0: ')
 
 # Muutetaan vastaukset liukuluvuiksi
-pituus = float(pituus_teksti) 
-paino = float(paino_teksti) 
+pituus = float(pituus_teksti)
+paino = float(paino_teksti)
 ika = float(ika_teksti)
 sukupuoli = float(sukupuoli_teksti)
 
@@ -27,10 +27,11 @@ def laske_bmi(paino, pituus):
     Returns:
         float: painoindeksi desimaalin tarkkuudella
     """
-    pituus = pituus / 100 # muutetaan pituus metreiksi
+    pituus = pituus / 100  # muutetaan pituus metreiksi
     bmi = paino / pituus**2
     bmi = round(bmi, 1)
     return bmi
+
 
 def aikuisen_rasvaprosentti(bmi, ika, sukupuoli):
     """_summary_
@@ -43,12 +44,27 @@ def aikuisen_rasvaprosentti(bmi, ika, sukupuoli):
     Returns:
         float: kehon rasvaprosentti (aikuinen)
     """
-    rasvaprosentti = 1.20 * bmi + 0.23 * ika - 10.8 * sukupuoli -5.4
+    rasvaprosentti = 1.20 * bmi + 0.23 * ika - 10.8 * sukupuoli - 5.4
     rasvaprosentti = round(rasvaprosentti)
     return rasvaprosentti
 
+def lapsen_rasvaprosentti(bmi, ika, sukupuoli):
+    """Laskee lapsen kehon rasvaprosentin
+
+    Args:
+        bmi (float): painoindeksi
+        ika (float): ikä
+        sukupuoli (float): poika -> 1, tyttö -> 0
+
+    Returns:
+        float: kehon rasvaprosentti (lapsi)
+    """
+    rasvaprosentti = 1.51 * bmi - 0.7 * ika - 3.6 * sukupuoli + 1.4
+    return rasvaprosentti
+
+
 oma_bmi = laske_bmi(paino, pituus)
-oma_rasvaprosentti = aikuisen_rasvaprosentti(oma_bmi, ika, sukupuoli)
+oma_rasvaprosentti = lapsen_rasvaprosentti(oma_bmi, ika, sukupuoli)
 
-print('Painoindeksisi on', oma_bmi, 'ja kehon rasvaprosentti on', oma_rasvaprosentti)
-
+print('Painoindeksisi on', oma_bmi,
+      'ja kehon rasvaprosentti on', oma_rasvaprosentti)
