9d4c42c4e72a0395d70c88ee5c05cd8fdc5139fc
Mika Vainio <koti.mika.vainio@gmail.com>
2023-02-03 10:33:09 +0200

    Aloitettu periytymisharjoitus

--- kuntoilija.py
+++ kuntoilija.py
@@ -11,6 +11,8 @@ import fitness
 
 class Kuntoilija:
     """Luokka kuntoilijan tietoja varten"""
+
+    # Olionmuodostin eli konstruktori
     def __init__(self, nimi, pituus, paino, ika, sukupuoli):
         
         # Määritellään tulevan olion ominaisuudet (property), luokan kentät (field)
@@ -19,12 +21,26 @@ class Kuntoilija:
         self.paino = paino
         self.ika = ika
         self.sukupuoli = sukupuoli
+        self.bmi = fitness.laske_bmi(self.paino, self.pituus)
+
+    # Metodi painoindeksin laskemiseen
+    def painoindeksi(self):
+        self.bmi = fitness.laske_bmi(self.paino, self.pituus)
+        return self.bmi
+
+    # Metodi rasvaprosentin laskemiseen (aikuinen)
+    def rasvaprosentti(self):
+        self.rasvaprosentti = fitness.aikuisen_rasvaprosentti(self.bmi,self.ika, self.sukupuoli)
+        return self.rasvaprosentti
 
 if __name__ == "__main__":
     
     # Luodaan oli luokasta Kuntoilija
     kuntoilija = Kuntoilija('Kalle Kuntoilija', 171, 65, 40, 1)
     print(kuntoilija.nimi, 'painaa', kuntoilija.paino, 'kg')
+    # print('painoindeksi on ', kuntoilija.painoindeksi())
+    print('rasvaprosentti on', kuntoilija.rasvaprosentti())
+
 
         
     
