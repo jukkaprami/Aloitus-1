514d049d6f428b1da8963c041b790798ef375461
Mika Vainio <koti.mika.vainio@gmail.com>
2023-02-03 12:52:13 +0200

    Lisätty luokka lapsille

--- kuntoilija.py
+++ kuntoilija.py
@@ -9,13 +9,16 @@ import fitness
 # LUOKKAMÄÄRITYKSET (CLASS DEFINITIONS)
 # -------------------------------------
 
+# Kuntoilija-luokka Yliluokka JunioriKuntoilijalle (super class)
+
+
 class Kuntoilija:
     """Luokka kuntoilijan tietoja varten"""
 
-    # Olionmuodostin eli konstruktori
+    # Olionmuodostin eli konstruktori, self -> tuleva olio
     def __init__(self, nimi, pituus, paino, ika, sukupuoli):
-        
-        # Määritellään tulevan olion ominaisuudet (property), luokan kentät (field)
+
+        # Määritellään tulevan olion ominaisuudet (property) eli luokan kentät (field)
         self.nimi = nimi
         self.pituus = pituus
         self.paino = paino
@@ -23,26 +26,40 @@ class Kuntoilija:
         self.sukupuoli = sukupuoli
         self.bmi = fitness.laske_bmi(self.paino, self.pituus)
 
-    # Metodi painoindeksin laskemiseen
-    def painoindeksi(self):
-        self.bmi = fitness.laske_bmi(self.paino, self.pituus)
-        return self.bmi
-
-    # Metodi rasvaprosentin laskemiseen (aikuinen)
+    # Metodi rasvaprosentin laskemiseen (yleinen / aikuinen)
     def rasvaprosentti(self):
-        self.rasvaprosentti = fitness.aikuisen_rasvaprosentti(self.bmi,self.ika, self.sukupuoli)
+        self.rasvaprosentti = fitness.aikuisen_rasvaprosentti(
+            self.bmi, self.ika, self.sukupuoli)
         return self.rasvaprosentti
 
+# JunioriKuntoilija-luokka Kuntoilija-luokan aliluokka (subclass)
+
+
+class JunioriKuntoilija(Kuntoilija):
+    """Luokka nuoren kuntoilijan tiedoille"""
+
+    # Konstruktori
+    def __init__(self, nimi, pituus, paino, ika, sukupuoli):
+
+        # Määritellään periytyminen, mitä ominaisuuksia perii
+        super().__init__(nimi, pituus, paino, ika, sukupuoli)
+
+    # Metodi rasvaprosentin laskemiseen (ylikirjoitettu lapsen metodilla)
+    def rasvaprosentti(self):
+        self.rasvaprosentti = fitness.lapsen_rasvaprosentti(
+            self.bmi, self.ika, self.sukupuoli)
+        return self.rasvaprosentti
+
+
 if __name__ == "__main__":
-    
+
     # Luodaan oli luokasta Kuntoilija
     kuntoilija = Kuntoilija('Kalle Kuntoilija', 171, 65, 40, 1)
     print(kuntoilija.nimi, 'painaa', kuntoilija.paino, 'kg')
     # print('painoindeksi on ', kuntoilija.painoindeksi())
     print('rasvaprosentti on', kuntoilija.rasvaprosentti())
 
-
-        
-    
-
-    
+    juniorikuntoilija = JunioriKuntoilija('Aku', 171, 65, 16, 1)
+    print(juniorikuntoilija.nimi, 'painaa', juniorikuntoilija.paino, 'kg')
+    # print('painoindeksi on ', kuntoilija.painoindeksi())
+    print('rasvaprosentti on', juniorikuntoilija.rasvaprosentti())
